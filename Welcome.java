bbb57af56ff1846ba01a1dbc3afcb0218d7d1254
N-g-a <97728232+N-g-a@users.noreply.github.com>
2022-03-31 20:07:51 +0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+package BT_14_1;
+
+public class Welcome {
+	public static void main(String[] args) {
+        //Diaplay message Welcome to Java! on the conscole
+        System.out.println("Welcome to Java!");
+    }
+}
+
